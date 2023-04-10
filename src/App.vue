9ba1357e3e40bb78b3895f481e839c61610dcdbd
Vanessa Gonzalez <vanessagpro5@gmail.com>
2023-04-10 14:06:27 -0600

    all changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header ref="addAnimation" class="align-header">
+        <div>
+           <img src="./assets/icon-weather.png">
+        </div>
+        <div class="header-text">
+           <h1 class="white-letters">Search your city here</h1>
+           <inputComponent/>
+           <button @click="showAnimation()" class="button-styling">Search</button>
+        </div>
+    </header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import inputComponent from '@/components/input-component.vue'
+  export default{
+    components: {inputComponent},
+    methods:{
+      showAnimation(){
+        this.$refs.addAnimation.classList.add("header-animation")
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @use "@/assets/global-mixins.scss";
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+  body{
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+  header{
+    width: 100vw;
+    height: 100vh;
+    background-color: #1282A2;
+  }
+  .align-header{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+  .white-letters{
+    color: #FEFCFB;
+    font-weight: 300;
+  }
+  .header-animation{
+    animation-name: reduce-header;
+    animation-fill-mode: forwards;
+    animation-duration: 3s;
+  }
+  .button-styling{
+    cursor: pointer;
+    @include global-mixins.inputAndButtonStyles;
+  }
+  @keyframes reduce-header{
+    from{ height: 100vh;}
+    to{height: 50vh;}
+  }
 </style>
---
+++ src/components/input-component.vue
@@ -0,0 +1,10 @@
+<template>
+    <input class="input-styling" type="text">
+</template>
+<style lang="scss">
+@use "@/assets/global-mixins.scss";
+    .input-styling{
+        width: 75%;
+        @include global-mixins.inputAndButtonStyles;
+    }
+</style>
